d86752188a17b2620f8b84143667c972c8fa011a
Subhan-Asghar <subhanasghar16@gmail.com>
2025-12-04 16:08:46 +0500

    Get user videos

--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -2,6 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@supabase/supabase-js"
 import { db } from "@/db/db";
 import { videos } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -9,63 +10,80 @@ const supabase = createClient(
 );
 
 export async function POST(req: NextRequest) {
-    try{
-         const id = req.headers.get("user-id")
-    if (!id) {
-        return NextResponse.json({ error: "Missing user ID" }, { status: 400 });
+    try {
+        const id = req.headers.get("user-id")
+        if (!id) {
+            return NextResponse.json({ error: "Missing user ID" }, { status: 400 });
+        }
+
+        const formData = await req.formData()
+
+
+        const video = formData.get("video") as File;
+        const poster = formData.get("poster") as File;
+        const videoTitle = formData.get("title") as string | null;
+
+        const videoBuffer = Buffer.from(await video.arrayBuffer());
+        const posterBuffer = Buffer.from(await poster.arrayBuffer());
+        const videoFileName = `${Date.now()}-${video.name}`;
+        const posterFileName = `${Date.now()}-poster.jpg`;
+
+        const { error: videoError } = await supabase.storage
+            .from("videos")
+            .upload(videoFileName, videoBuffer, {
+                contentType: video.type,
+            });
+
+        if (videoError) throw videoError;
+
+        const { error: posterError } = await supabase.storage
+            .from("posters")
+            .upload(posterFileName, posterBuffer, {
+                contentType: "image/jpeg",
+            });
+
+        if (posterError) throw posterError;
+
+        const { data: videoUrlData } = supabase.storage
+            .from("videos")
+            .getPublicUrl(videoFileName);
+
+        const { data: posterUrlData } = supabase.storage
+            .from("posters")
+            .getPublicUrl(posterFileName);
+
+        const videoUrl = videoUrlData.publicUrl;
+        const posterUrl = posterUrlData.publicUrl;
+
+        const uuid = crypto.randomUUID();
+        await db.insert(videos).values({
+            id: uuid,
+            videoTitle: videoTitle,
+            videoUrl: videoUrl,
+            posterUrl: posterUrl,
+            user_id: id
+        })
+        return NextResponse.json({ messgae: "Upload successful" }, { status: 200 })
+    }
+    catch {
+        return NextResponse.json({ message: "Server Error" }, { status: 500 })
     }
 
-    const formData = await req.formData()
+}
 
+export async function GET(req: NextRequest) {
+    try {
+        const id = req.headers.get("user-id")
+        if (!id) {
+            return NextResponse.json({ error: "Missing user ID" }, { status: 400 });
+        }
 
-    const video = formData.get("video") as File;
-    const poster = formData.get("poster") as File;
-    const videoTitle = formData.get("title") as string | null;
-
-    const videoBuffer = Buffer.from(await video.arrayBuffer());
-    const posterBuffer = Buffer.from(await poster.arrayBuffer());
-    const videoFileName = `${Date.now()}-${video.name}`;
-    const posterFileName = `${Date.now()}-poster.jpg`;
-
-    const { error: videoError } = await supabase.storage
-        .from("videos")
-        .upload(videoFileName, videoBuffer, {
-            contentType: video.type,
-        });
-
-    if (videoError) throw videoError;
-
-    const { error: posterError } = await supabase.storage
-        .from("posters")
-        .upload(posterFileName, posterBuffer, {
-            contentType: "image/jpeg",
-        });
-
-    if (posterError) throw posterError;
-
-    const { data: videoUrlData } = supabase.storage
-        .from("videos")
-        .getPublicUrl(videoFileName);
-
-    const { data: posterUrlData } = supabase.storage
-        .from("posters")
-        .getPublicUrl(posterFileName);
-
-    const videoUrl = videoUrlData.publicUrl;
-    const posterUrl = posterUrlData.publicUrl;
-
-    const uuid = crypto.randomUUID();
-    await db.insert(videos).values({
-        id: uuid,
-        videoTitle: videoTitle,
-        videoUrl: videoUrl,
-        posterUrl: posterUrl,
-        user_id: id
-    })
-    return NextResponse.json({messgae:"Upload successful"},{status:200})
+        const data=await db.select().from(videos).where(eq(videos.user_id,id))
+        return NextResponse.json({
+            data:data
+        },{status:200})
     }
-    catch{
-        return NextResponse.json({message:"Server Error"},{status:500})
+    catch {
+        return NextResponse.json({ message: "Server Error" }, { status: 500 })
     }
-   
 }
