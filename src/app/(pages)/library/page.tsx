dde163b32e70b9ac2baa81f147626b2d0e5d32c1
Subhan-Asghar <subhanasghar16@gmail.com>
2025-12-04 12:53:55 +0500

    VideoCard

--- src/app/(pages)/library/page.tsx
+++ src/app/(pages)/library/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Logout from '@/components/auth/logout'
+import { VideoCard } from '@/components/library/video-card'
 const page = () => {
+ 
   return (
     <>
     Library
     <Logout></Logout>
+    {/* <VideoCard src='https://youtu.be/O5HQ1sZseKg?si=PD6RAlkKKt2o1rkB' title='Music' poster='./vercel.svg'></VideoCard> */}
     </>
   )
 }
---
+++ src/components/library/video-card.tsx
@@ -0,0 +1,48 @@
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { HeroVideoDialog } from "@/components/ui/hero-video-dialog"
+import { CirclePlay } from 'lucide-react';
+
+type VideoProps = {
+    title: string,
+    poster: string,
+    src: string
+}
+
+export function VideoCard({ title, poster, src }: VideoProps) {
+    return (
+        <>
+            <Card className="max-w-sm m-3 pb-1 ">
+                <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                    <CardAction className="cursor-pointer"><CirclePlay size={"25"} className="hover:text-primary" /></CardAction>
+                </CardHeader>
+                <CardContent className="px-1 ">
+                    <div className="relative ">
+                        <HeroVideoDialog
+                            className="block dark:hidden"
+                            animationStyle="top-in-bottom-out"
+                            videoSrc={src}
+                            thumbnailSrc={poster}
+                            thumbnailAlt={title}
+                        />
+                        <HeroVideoDialog
+                            className="hidden dark:block "
+                            animationStyle="top-in-bottom-out"
+                            videoSrc={src}
+                            thumbnailSrc={poster}
+                            thumbnailAlt={title}
+                        />
+                    </div>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
